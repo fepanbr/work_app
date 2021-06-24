5a05718bb3fbf50ab0154079c01923e7c0868c6a
fepanbr <fepanbr@gmail.com>
2021-06-24 23:33:40 +0900

    [기능추가] modify페이지 출근시간, 퇴근시간 수정 기능 추가중
    - timepicker 적용

--- lib/pages/modifyPage.dart
+++ lib/pages/modifyPage.dart
@@ -1,4 +1,8 @@
+import 'package:commute_app/constants.dart';
+import 'package:commute_app/models/work.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class ModifyPage extends StatefulWidget {
   @override
@@ -6,6 +10,27 @@ class ModifyPage extends StatefulWidget {
 }
 
 class _ModifyPageState extends State<ModifyPage> {
+  Future<DateTime?> _selectTime(BuildContext context, DateTime time) async {
+    var selectedTime = TimeOfDay.fromDateTime(time);
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+    );
+    if (picked != null) {
+      var _hour;
+      var _minute;
+      DateTime result;
+      selectedTime = picked;
+      _hour = selectedTime.hour;
+      _minute = selectedTime.minute;
+      result = DateTime(time.year, time.month, time.day, _hour, _minute);
+      return result;
+    } else {
+      return null;
+    }
+  }
+
+  Work work = Get.arguments;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +39,182 @@ class _ModifyPageState extends State<ModifyPage> {
         title: Text('Work App'),
         centerTitle: true,
         brightness: Brightness.dark,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              width: double.infinity,
+              height: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Text(
+                      '출근시간',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      work.startWorkTime,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        var result = await _selectTime(
+                            context, Work.toDateTime(work.startTime));
+                        setState(() {
+                          if (result != null) {
+                            work.startTime =
+                                DateFormat("yyyyMMddHHmm").format(result);
+                          }
+                        });
+                      },
+                      child: Text('수정하기'),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(kPrimaryColor),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              width: double.infinity,
+              height: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Text(
+                      '퇴근시간',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      work.endWorkTime,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        var result = await _selectTime(
+                            context, Work.toDateTime(work.endTime));
+                        setState(() {
+                          if (result != null) {
+                            work.endTime =
+                                DateFormat("yyyyMMddHHmm").format(result);
+                          }
+                        });
+                      },
+                      child: Text(
+                        '수정하기',
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(kPrimaryColor),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              width: double.infinity,
+              height: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Text(
+                      '식사시간',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      work.mealTimeToString,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('수정하기'),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(kPrimaryColor),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              width: double.infinity,
+              height: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Text(
+                      '연차유무',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      work.onLeaveText,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        '수정하기',
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(kPrimaryColor),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
