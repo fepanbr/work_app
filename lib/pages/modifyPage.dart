258233c05c5dcc18526b20752ed82167af0848cb
fepanbr <fepanbr@gmail.com>
2021-06-26 17:36:56 +0900

    [버그] numberpicker 제거

--- lib/pages/modifyPage.dart
+++ lib/pages/modifyPage.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
-import 'package:numberpicker/numberpicker.dart';
 
 class ModifyPage extends StatefulWidget {
   @override
